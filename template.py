27cfeb3b170833700c99bdb222304b69b0f1da4c
anfeher <ahdez0905@gmail.com>
2024-03-14 16:42:31 -0500

    add folder structure

---
+++ template.py
@@ -0,0 +1,48 @@
+import os
+import logging
+
+from pathlib import Path
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s"
+)
+
+project_name = "text_summarization"
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/logging/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "research/trials.ipynb",
+    "config/config.yml",
+    "params.yml",
+    "app.py",
+    "main.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py"
+]
+
+for file_path in list_of_files:
+    file_path = Path(file_path)
+    file_dir, file_name = os.path.split(file_path)
+
+    if file_dir != "":
+        os.makedirs(file_dir, exist_ok=True)
+        logging.info(f"Creating folder {file_dir}")
+    
+    if not os.path.exists(file_path):
+        with open(file_path, "w") as f:
+            pass
+        logging.info(f"Creating empty file: {file_path}")
+    else:
+        logging.info(f"{file_path} already exists")
