1a3dbd4d644aacca05c576b7c2482dba6a7bc63d
anfeher <ahdez0905@gmail.com>
2024-03-14 22:15:21 -0500

    add requirements and setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+AUTHOR_USER_NAME = "anfeher"
+AUTHOR_EMAIL = "ahdez0905@gmail.com"
+REPO_NAME = "Text-Summarization"
+SRC_REPO = "text_summarization"
+
+setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    package_dir={"":"src"},
+    packages=find_packages(where="src")
+)
